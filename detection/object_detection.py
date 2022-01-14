531376a9ec6d23a757d132dc2b959f53fe7a9668
VenkataNarayana Bommanaboina <56188562+venkatanarayana143@users.noreply.github.com>
2022-01-15 02:09:08 +0530

    Update object_detection.py

--- detection/object_detection.py
+++ detection/object_detection.py
@@ -3,9 +3,9 @@ import numpy as np
 import os
 
 def detect_object(frame):
-    cfg_path = os.path.abspath('yolo/yolov4.cfg')
+    cfg_path = os.path.abspath('yolo/yolov4_custom.cfg')
     weights_path = os.path.abspath('yolov4.weights')
-    names_path = os.path.abspath('yolo/coco.names')
+    names_path = os.path.abspath('yolo/obj.names')
 
     # Load Yolo
     # net = cv2.dnn_DetectionModel('yolov4.cfg', 'yolov4.weights')
